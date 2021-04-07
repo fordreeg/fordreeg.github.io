a31416bd4e65db259e3932fc30c020f3ed5b4d65
vlad <fordreej@gmail.com>
2021-04-07 16:00:32 +0300

    reduce photo

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,11 +1,30 @@
 const hamburger = document.querySelector('.hamburger'),
       menu = document.querySelector('.menu'),
+      menuList = document.querySelectorAll('.menu__list-item'),
+      menuIcon = document.querySelectorAll('.menu__icon'),
       closeElem = document.querySelector('.menu__close');
 
 hamburger.addEventListener('click', () => {
     menu.classList.add('active');
+    document.body.style.overflow = 'hidden';
 });
+
 
 closeElem.addEventListener('click', () => {
     menu.classList.remove('active');
+    document.body.style.overflow = '';
+});
+
+menuList.forEach(item => {
+    item.addEventListener('click', () => {
+        menu.classList.remove('active');
+        document.body.style.overflow = '';
+    })
+});
+
+menuIcon.forEach(item => {
+    item.addEventListener('click', () => {
+        menu.classList.remove('active');
+        document.body.style.overflow = '';
+    })
 });
