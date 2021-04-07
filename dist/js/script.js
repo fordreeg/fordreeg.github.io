32b2360fc6f1f17427724e2b35c61cf14c8acd0b
vlad <fordreej@gmail.com>
2021-04-07 22:17:30 +0300

    add phpMailer

--- dist/js/script.js
+++ dist/js/script.js
@@ -28,3 +28,44 @@ menuIcon.forEach(item => {
         document.body.style.overflow = '';
     })
 });
+
+
+const overlay = document.querySelector('.overlay'),
+          modalClose = document.querySelector('.modal__close'),
+          modal = document.querySelector('.modal');
+
+
+// Отправка данных на сервер
+function send(event, php){
+    console.log("Отправка запроса");
+    event.preventDefault ? event.preventDefault() : event.returnValue = false;
+    var req = new XMLHttpRequest();
+    req.open('POST', php, true);
+    req.onload = function() {
+        if (req.status >= 200 && req.status < 400) {
+        json = JSON.parse(this.response);
+            console.log(json);
+            
+            if (json.result == "success") {
+                overlay.style.display = "block";
+                modal.style.display = "block";
+                document.body.style.overflow = 'hidden';
+                document.getElementById('name').value = "";
+                document.getElementById('mail').value = "";
+                document.getElementById('message').value = "";
+
+            } else {
+                alert("Ошибка. Сообщение не отправлено");
+            }
+        } else {alert("Ошибка сервера. Номер: "+req.status);}}; 
+    
+    // Если не удалось отправить запрос. Стоит блок на хостинге
+    req.onerror = function() {alert("Ошибка отправки запроса");};
+    req.send(new FormData(event.target));
+    };
+
+    modalClose.addEventListener('click', () =>{
+        overlay.style.display = "";
+        modal.style.display = "";
+        document.body.style.overflow = '';
+    });
