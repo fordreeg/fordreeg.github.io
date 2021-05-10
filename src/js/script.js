5e298fd11e202250e4e018a0d89306fbea85b917
vlad <fordreej@gmail.com>
2021-05-10 17:58:51 +0300

    refactirong and redesign

--- src/js/script.js
+++ src/js/script.js
@@ -1,52 +1,34 @@
-const hamburger = document.querySelector('.hamburger'),
-      menu = document.querySelector('.menu'),
-      menuList = document.querySelectorAll('.menu__list-item'),
-      menuIcon = document.querySelectorAll('.menu__icon'),
-      closeElem = document.querySelector('.menu__close');
-
-hamburger.addEventListener('click', () => {
-    menu.classList.add('active');
-    document.body.style.overflow = 'hidden';
-});
-
-
-closeElem.addEventListener('click', () => {
-    menu.classList.remove('active');
-    document.body.style.overflow = '';
-});
-
-menuList.forEach(item => {
-    item.addEventListener('click', () => {
-        menu.classList.remove('active');
-        document.body.style.overflow = '';
-    })
-});
-
-menuIcon.forEach(item => {
-    item.addEventListener('click', () => {
-        menu.classList.remove('active');
-        document.body.style.overflow = '';
-    })
-});
-
-
 const overlay = document.querySelector('.overlay'),
-          modalClose = document.querySelector('.modal__close'),
-          modal = document.querySelector('.modal');
+      modalClose = document.querySelector('.modal-close'),
+      modal = document.querySelector('.modal');
 
+document.body.addEventListener('click', (e) => {
+    if(e.target === modalClose || e.target === overlay) {
+        overlay.style.display = "none";
+        modal.style.display = "none";
+        document.body.style.overflow = 'unset';
+    }
+
+});
 
 // Отправка данных на сервер
 function send(event, php){
     console.log("Отправка запроса");
+
     event.preventDefault ? event.preventDefault() : event.returnValue = false;
-    var req = new XMLHttpRequest();
+
+    let req = new XMLHttpRequest();
+
     req.open('POST', php, true);
     req.onload = function() {
+
         if (req.status >= 200 && req.status < 400) {
-        json = JSON.parse(this.response);
+
+            let json = JSON.parse(this.response);
+
             console.log(json);
             
-            if (json.result == "success") {
+            if (json.result === "success") {
                 overlay.style.display = "block";
                 modal.style.display = "block";
                 document.body.style.overflow = 'hidden';
@@ -57,15 +39,15 @@ function send(event, php){
             } else {
                 alert("Ошибка. Сообщение не отправлено");
             }
-        } else {alert("Ошибка сервера. Номер: "+req.status);}}; 
+        } else {
+            alert("Ошибка сервера. Номер: "+req.status);
+        }
+    };
     
     // Если не удалось отправить запрос. Стоит блок на хостинге
-    req.onerror = function() {alert("Ошибка отправки запроса");};
-    req.send(new FormData(event.target));
+    req.onerror = function() {
+        alert("Ошибка отправки запроса");
     };
 
-    modalClose.addEventListener('click', () =>{
-        overlay.style.display = "";
-        modal.style.display = "";
-        document.body.style.overflow = '';
-    });
+    req.send(new FormData(event.target));
+}
